3cfc301d3c3b5e4d5da6290fae856a302f6babaf
Daniele Teti <d.teti@bittime.it>
2024-11-28 09:31:27 +0100

    first commit

---
+++ ControllerU.pas
@@ -0,0 +1,40 @@
+unit ControllerU;
+
+interface
+
+uses
+  MVCFramework, MVCFramework.Commons, MVCFramework.Serializer.Commons, System.Generics.Collections;
+
+type
+  [MVCPath]
+  TMyController = class(TMVCController)
+  public
+    [MVCPath]
+    [MVCHTTPMethod([httpGET])]
+    [MVCProduces(TMVCMediaType.TEXT_HTML)]
+    function Index: String;
+
+    [MVCPath('/reversedstrings')]
+    [MVCHTTPMethod([httpGET])]
+    [MVCProduces(TMVCMediaType.TEXT_PLAIN)]
+    function GetReversedString(const [MVCFromQueryString('value','')] Value: String): String;
+
+  end;
+
+implementation
+
+uses
+  System.StrUtils, System.SysUtils, MVCFramework.Logger;
+
+
+function TMyController.Index: String;
+begin
+  Result := Page('index');
+end;
+
+function TMyController.GetReversedString(const Value: String): String;
+begin
+  Result := System.StrUtils.ReverseString(Value.Trim);
+end;
+
+end.
